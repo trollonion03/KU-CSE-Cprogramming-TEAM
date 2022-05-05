7c3ea7a2a566088514413bc884ce876f7e06c497
Trollonion03 <trollonion03@gmail.com>
2022-05-05 23:39:22 +0900

    Some functions and Explanation updated

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 /************************************************************
-C programming Team project(Treasure hunt Game) by Team 7
-https://github.com/trollonion03/Cpre_Tp1
-First build: May 3rd, 2022
-Last build: May 5th, 2022
-
-Target: Windows(x86-64)
-Language : C(MSVC, v142)
+*C programming Team project(Treasure hunt Game) by Team 7
+*https://github.com/trollonion03/Cpre_Tp1
+*First build: May 3rd, 2022
+*Last build: May 5th, 2022
+*
+*Target: Windows(x86-64)
+*Language : C(MSVC, v142)
 ************************************************************/
 
-//#pragma warning(disable:6031)
+//#pragma warning(disable:6031) //for scanf
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <Windows.h>
@@ -34,7 +34,10 @@ void sel_lv();
 
 
 int main() {
-	//TODO: Modularize it as much as possible
+	/***********************************************
+	*Modularize it as much as possible
+	*Use this function to import other functions
+	***********************************************/
 	int gch;
 	
 	init();
@@ -74,6 +77,7 @@ void CreateTitleScreen() {
 }
 
 void sel_lv() {
+	//TODO: New design needed
 	int gch1;
 	while (1) {
 		system("cls");
@@ -113,12 +117,18 @@ void Game_Core(int lvs) {
 	int ground[25][15];
 	system("cls");
 	printf("--------------------------------------------------------\n");
-	if (lvs == 1)
+	if (lvs == 1) {
+		gotoxy(25, 1);
 		printf("쉬움\n");
-	else if (lvs == 2)
+	}	
+	else if (lvs == 2) {
+		gotoxy(25, 1);
 		printf("보통\n");
-	else if (lvs == 3)
+	}
+	else if (lvs == 3) {
+		gotoxy(24, 1);
 		printf("어려움\n");
+	}	
 	printf("--------------------------------------------------------\n");
 	Create_Ground();
 
@@ -126,6 +136,7 @@ void Game_Core(int lvs) {
 }
 
 void movekey() {
+	//TODO: Move function to another function
 	int x = 0, y = 0, ch;
 	gotoxy(x, y);
 	ch = _getch();
@@ -156,9 +167,11 @@ void movekey() {
 }
 
 void Create_Ground() {
-	//if you use a emoji like '■', the emojis consume 2 spaces.
-	//If you want to move one coordinate, you have to move two spaces.
-	//TODO: Find the right ground size
+	/******************************************************************
+	*if you use a emoji like '■', the emojis consume 2 spaces.
+	*If you want to move one coordinate, you have to move two spaces.
+	*TODO: Find the right ground size
+	******************************************************************/
 	int i, j;
 	int x = 12; //width
 	int y = 12; //height
